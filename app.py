217f4fceaa79a076e76cc487002dfc18b711cb2d
Shaurya Fulfagar <80444101+ShauryaFulfagar@users.noreply.github.com>
2024-05-08 23:24:30 +0530

    Added Everything

---
+++ app.py
@@ -0,0 +1,68 @@
+# app.py
+
+from flask import Flask, render_template, redirect, url_for, request
+import firebase_admin
+from firebase_admin import credentials, firestore, auth
+import extData
+
+app = Flask(__name__)
+
+# Initialize Firebase Admin SDK with your credentials
+cred = credentials.Certificate('./firebase.json')
+firebase_admin.initialize_app(cred)
+
+# Initialize Firestore DB
+db = firestore.client()
+
+@app.route('/')
+def homePage():
+    return render_template('index.html')
+
+# Route for the authentication page
+@app.route('/auth')
+def authPage():
+    return render_template('auth.html')
+
+
+@app.route('/contact')
+def contactPage():
+    return render_template('contact.html')
+# Route for the find page
+@app.route('/find')
+def findPage():
+    # Check if user is authenticated
+    try:
+        id_token = request.cookies.get("token")
+        decoded_token = auth.verify_id_token(id_token)
+        user_id = decoded_token['uid']
+
+        # Retrieve data from Firestore
+        docs = db.collection('cards').list_documents()
+
+        data = []
+        for doc in docs:
+            doc_data = doc.get().to_dict()
+            data.append(doc_data)
+
+        return render_template('find.html', data=data)
+
+    except Exception as e:
+        # Redirect to authentication page if user is not authenticated
+        return redirect(url_for('authPage'))
+
+# Function to push data to Firestore
+def push_data_to_firestore(data):
+    for idx, item in enumerate(data):
+        doc_ref = db.collection('cards').document(f'card-{idx + 1}')
+        doc_ref.set(item)
+
+    print("Data has been successfully pushed to Firestore.")
+
+# Sample data
+rawData = extData.rawData
+
+# Push sample data to Firestore
+push_data_to_firestore(rawData)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ extData.py
@@ -0,0 +1,84 @@
+rawData = [
+    {
+        "name": "Vihaan Singh",
+        "description ": "Enthusiastic learner with a passion for technology and innovation.",
+        "expertise 1": "Software Development",
+        "expertise 2": "Web Design",
+        "expertise 3 ": "Mobile App Development",
+        "backgroundPic ": "https://images.pexels.com/photos/531880/pexels-photo-531880.jpeg",
+        "profilePic": "https://images.pexels.com/photos/4295983/pexels-photo-4295983.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        "name": "Priya Patel",
+        "description ": "Detail-oriented professional with a strong work ethic and excellent communication skills.",
+        "expertise 1": "Marketing Strategy",
+        "expertise 2": "Social Media Management",
+        "expertise 3 ": "Content Marketing",
+        "backgroundPic ": "https://images.pexels.com/photos/707915/pexels-photo-707915.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "profilePic": "https://images.pexels.com/photos/1520760/pexels-photo-1520760.jpeg?auto=compress&cs=tinysrgb&w=600"
+    },
+    {
+        "name": "Arjun Kumar",
+        "description ": "Results-driven individual with a focus on achieving goals and exceeding expectations.",
+        "expertise 1": "Financial Analysis",
+        "expertise 2": "Investment Banking",
+        "expertise 3 ": "Risk Management",
+        "backgroundPic ": "https://images.pexels.com/photos/355872/pexels-photo-355872.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "profilePic": "https://images.pexels.com/photos/17136147/pexels-photo-17136147/free-photo-of-indian-traditional-beard-man.jpeg?auto=compress&cs=tinysrgb&w=600"
+    },
+    {
+        "name": "Aisha Sharma",
+        "description ": "Team player with a positive attitude and a commitment to continuous improvement.",
+        "expertise 1": "Project Management",
+        "expertise 2": "Agile Methodologies",
+        "expertise 3 ": "Scrum Framework",
+        "backgroundPic ": "https://images.pexels.com/photos/355770/pexels-photo-355770.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "profilePic": "https://images.pexels.com/photos/7515082/pexels-photo-7515082.jpeg?auto=compress&cs=tinysrgb&w=600"
+    },
+    {
+        "name": "Vikram Gupta",
+        "description ": "Results-driven individual with a focus on achieving goals and exceeding expectations.",
+        "expertise 1": "Graphic Design",
+        "expertise 2": "UX/UI Design",
+        "expertise 3 ": "Brand Identity",
+        "backgroundPic ": "https://images.pexels.com/photos/22794/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "profilePic": "https://images.pexels.com/photos/4276830/pexels-photo-4276830.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        "name": "Kavita Joshi",
+        "description ": "Analytical thinker with a passion for data-driven decision-making.",
+        "expertise 1": "Customer Service",
+        "expertise 2": "Conflict Resolution",
+        "expertise 3 ": "Negotiation Skills",
+        "backgroundPic ": "https://images.pexels.com/photos/1450353/pexels-photo-1450353.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        "profilePic": "https://images.pexels.com/photos/2690323/pexels-photo-2690323.jpeg?auto=compress&cs=tinysrgb&w=600"
+    },
+    {
+        "name": "Siddharth Shah",
+        "description ": "Organized and resourceful individual with a dedication to delivering high-quality work.",
+        "expertise 1": "Data Analysis",
+        "expertise 2": "Market Research",
+        "expertise 3 ": "Statistical Analysis",
+        "backgroundPic ": "https://images.pexels.com/photos/315998/pexels-photo-315998.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        "profilePic": "https://images.pexels.com/photos/10311994/pexels-photo-10311994.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        "name": "Nandini Choudhary",
+        "description ": "Adaptable and versatile professional with a willingness to take on new challenges.",
+        "expertise 1": "Content Writing",
+        "expertise 2": "Copywriting",
+        "expertise 3 ": "SEO Optimization",
+        "backgroundPic ": "https://images.pexels.com/photos/1252890/pexels-photo-1252890.jpeg?auto=compress&cs=tinysrgb&w=600",
+        "profilePic": "https://images.pexels.com/photos/22601971/pexels-photo-22601971/free-photo-of-woman-portrait-in-black-and-white.jpeg?auto=compress&cs=tinysrgb&w=600"
+    },
+    {
+        "name": "Rohan Verma",
+        "description ": "Strategic thinker with a talent for developing innovative solutions.",
+        "expertise 1": "Human Resources",
+        "expertise 2": "Talent Acquisition",
+        "expertise 3 ": "Performance Management",
+        "backgroundPic ": "https://images.pexels.com/photos/39003/scotland-united-kingdom-england-isle-of-skye-39003.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        "profilePic": "https://images.pexels.com/photos/7752846/pexels-photo-7752846.jpeg?auto=compress&cs=tinysrgb&w=600"
+    }
+
+]
